2890992ccfcf4b9aa6405f9eda7b400719817381
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-11 20:32:59 +0300

    Props types

---
+++ types/index.ts
@@ -0,0 +1,9 @@
+import { MouseEventHandler } from 'react';
+
+export interface TaskProps {
+	id: number;
+	task: string;
+	status: 'uncompleted' | 'completed';
+	complete: (id: number) => void;
+	remove: (id: number) => void;
+}
