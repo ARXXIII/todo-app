a47fcc3c044d1c9e2d0255696993ef16fea92f57
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-12 13:10:14 +0300

    Header changes

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,14 @@
 export const Header = () => {
     return (
         <header className="flex justify-center lg:justify-between items-center gap-x-12 font-black text-4xl tracking-wide shrink-0">
-            <h1>AR23 | TODO App</h1>
-            <a href="https://github.com/PICKLEGENT/todo-app" target="_blanc" className="hidden lg:block bg-gradient-to-r from-black to-green-500 text-transparent bg-clip-text">GitHub репозиторий</a>
+            <h1 className="bg-gradient-to-r from-yellow-400 to-purple-800 text-transparent bg-clip-text">AR23 | TODO App</h1>
+            <a
+                href="https://github.com/PICKLEGENT/todo-app"
+                target="_blanc"
+                className="hidden lg:block bg-gradient-to-r from-github-dark to-github-green text-transparent bg-clip-text"
+            >
+                GitHub репозиторий
+            </a>
         </header>
     )
 }
