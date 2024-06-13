48a156deb4139da73741a0ad4d50cb1e3b88b53c
Андрей Романов <human1488pwnz@yandex.ru>
2024-06-13 17:07:19 +0300

    Footer changes Added instagram link

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,17 +7,18 @@ export const Footer = () => {
                     <h2 className="text-xl">2024 Edition</h2>
                 </div>
                 <div className="flex flex-col items-center lg:items-start gap-y-3">
-                    <h2 className="font-bold text-sm uppercase">полезные ссылочки</h2>
+                    <h2 className="font-bold text-sm uppercase">socials</h2>
                     <div className="flex gap-x-3 text-xl">
                         <a href="https://github.com/PICKLEGENT" target="_blanc">GitHub</a>
                         <a href="https://t.me/AndrewRom" target="_blanc">Telegram</a>
+                        <a href="https://www.instagram.com/picklegent?igsh=Ymt3N3o5dWpnYm5u&utm_source=qr" target="_blanc">Instagram</a>
                     </div>
                 </div>
             </section>
             <div className="hidden lg:flex justify-center items-center mt-6 text-center">
                 <a
                     href="https://github.com/PICKLEGENT"
-                    title='Главный по раскрашиванию кнопочек'
+                    title='Chief of button coloring'
                     target="_blanc"
                     className="text-sm text-neutral-500 leading-relaxed"
                 >
