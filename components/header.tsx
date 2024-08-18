ddf334941cc43e5a434b79fbe93062759f8e5d87
Андрей Романов <human1488pwnz@yandex.ru>
2024-08-18 14:14:15 +0300

    Change logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <header className="flex justify-center lg:justify-between items-center gap-x-12 font-black text-4xl tracking-wide shrink-0">
-            <h1 className="bg-gradient-to-r from-yellow-400 to-purple-800 text-transparent bg-clip-text">AR23 | TODO App</h1>
+            <h1 className="bg-gradient-to-r from-yellow-400 to-purple-800 text-transparent bg-clip-text">ARXXIII | TODO App</h1>
         </header>
     )
 }
